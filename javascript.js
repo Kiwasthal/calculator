1836c2967e4561e6be62b3ac91fe20aba06d7d57
Steerpike <imthekoul@gmail.com>
2022-03-16 16:11:04 +0200

    Calculater V1, clicked interface completely integrated, start bug-testing

--- javascript.js
+++ javascript.js
@@ -17,7 +17,7 @@ function createOperation(name){
 
 //Initialize praxis object to store variables and operator
 
-const praxis = createOperation('praxis');
+let praxis = createOperation('praxis');
 praxis.operator = ""
 
 //Create a function that takes two numbers and an operator and calls one of the above functions depending on the operator
@@ -59,28 +59,75 @@ let buttons = document.querySelectorAll('button')
 //Creating variables for different inputs
 
 let logText = (e) => {
-  
-  if (display.length < 9 && parseInt(e.target.textContent) >= 0 && praxis.operator === "" ) {
-    display += e.target.textContent;
+ 
+// Clear all variables of object praxis if C is clicked 
+
+  if(e.target.textContent == 'C' ){
+          praxis.status = false
+          praxis.numOne = ""
+          praxis.numTwo = ""
+          praxis.operator = ""
+          screen.textContent = ""
+          display = ""   
+  }
+
+//Check for status(enables stringing together operations), create variables for praxis number, depending on status-state
+
+ else if (display.length < 9 && parseInt(e.target.textContent) >= 0 && praxis.operator === "" ) {
+    if (praxis.status === true){ 
+      display += e.target.textContent;
+      screen.textContent = display;
+      praxis.numTwo = display;
+    } else if (!praxis.status == true) {
+        display += e.target.textContent;
+        screen.textContent = display;
+        praxis.numOne = display;
+    }        
+  }
+// Inserts a dot and to create floaters!!Should disable after one click
+
+  else if (e.target.textContent === "," ) {
+    display += ".";
     screen.textContent = display;
     praxis.numOne = display;
-} else if( e.target.textContent == "+" || e.target.textContent =="-"
+  }
+  
+//
+
+// Depending on status state we create the variable in praxis needed to perform operations
+
+  else if( e.target.textContent == "+" || e.target.textContent =="-"
   || e.target.textContent =="×"|| e.target.textContent =="÷") { 
-    praxis.operator = e.target.textContent;
-    display = "";
-    praxis.status = 'active';
-    screen.textContent = display
+    if (praxis.status === true) {
+      let result = operate(praxis.operator,parseFloat(praxis.numOne),parseFloat(praxis.numTwo));
+      praxis.operator = e.target.textContent
+      praxis.numOne = result
+      screen.textContent = result
+      display = ""
+    }   else if (!praxis.status === true) {
+          praxis.operator = e.target.textContent;
+          display = "";
+          screen.textContent = display;
+          praxis.status = true ; //Praxis.status enables to chain multiple operations without clicking equal , if operator button is clicked once, then status activates
+    }
+// Create variables for num.2 . !! Works if status - not set-
+
 } else if ( display.length < 9 && parseInt(e.target.textContent) >= 0 ){
     display += e.target.textContent;
     screen.textContent = display;
     praxis.numTwo = display;
+
+//Calculates result if "=" is clicked, also prevents larger numbers from floating the screen    
+
 } else if (e.target.textContent == "=") {
-    let result = operate(praxis.operator,parseInt(praxis.numOne),parseInt(praxis.numTwo));
+    let result = operate(praxis.operator,parseFloat(praxis.numOne),parseFloat(praxis.numTwo));
     if (toString(result).length > 9) {
     console.log(result)
     result = result.toString().slice(0,10)
     screen.textContent = result
     praxis.numOne = result
+    display = ''
+    praxis.status = false
     }
     else {
     screen.textContent = result
@@ -90,11 +137,24 @@ let logText = (e) => {
 return praxis;
   }
 
+  //Find sound depending on key pressed and added animation class 'pressed'
 let findSound = (e) => {
     let audio = document.getElementById('keysound')
+    e.target.classList.add('pressed')
     audio.currentTime = 0;
     audio.play();
 }
+ //removes transition after mouse lift 
+
+window.addEventListener('mouseup', (e) => {
+  e.target.classList.remove('pressed')
+})  
+
 
 buttons.forEach(button => button.addEventListener('click',logText));
-buttons.forEach(button => button.addEventListener('click',findSound));
+buttons.forEach(button => button.addEventListener('mousedown',findSound));
+
+
+window.addEventListener('mouseup', (e) => {
+  e.target.classList.remove('pressed')
+})  
